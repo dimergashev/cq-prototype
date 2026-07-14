99c92042ebbdc6b2966a872e58f8f742485d2311
dimergashev <dim@skrooge.ai>
2026-07-14 13:58:27 +0200

    deploy: 2026-07-14 — Main prototype: only Savings lifecycle (1) + (multi) v2 tabs

---
+++ assets/hero-character.DNp7BIdW.js
@@ -0,0 +1,1 @@
+const r="/cq-prototype/assets/hero-character.BPI-8xCG.svg";export{r as h};
---
+++ assets/index.CycOmUDD.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index.W3odvRjD.js";var s={name:"OverlayPanel",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Popover component instead.")}};export{s};
